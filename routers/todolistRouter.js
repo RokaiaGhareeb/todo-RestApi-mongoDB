f887da4806bc5d078b8c2f5b48584e6b42297521
Rokaia Ghareeb <Rokaiaessam97@gmail.com>
2021-03-15 22:21:33 +0200

    status

--- routers/todolistRouter.js
+++ routers/todolistRouter.js
@@ -16,42 +16,88 @@ todolistRoute.use(auth);
 todolistRoute.post('/', async (req, res) => {
     const { title } = req.body;
     try {
-      const todolist = await TodoList.create({ userId: req.signedData.id, title, listItems : []});
-      res.statusCode = 200;
-      res.send(todolist);
+        const todolist = await TodoList.create({ userId: req.signedData.id, title, listItems: [] });
+        res.statusCode = 200;
+        res.send(todolist);
     } catch (err) {
-      res.statusCode = 422;
-      res.send({ success: false });
+        res.statusCode = 422;
+        res.send({ success: false });
     }
-  });
+});
 
 //add new item to the list
 todolistRoute.post('/item/:id', async (req, res) => {
     const id = req.params.id;
     const { title, done } = req.body;
     try {
-      const todolist = await TodoList.updateOne(
-        { _id: id },
-        { $push: { listItems : { title, done } } }
-     );
+        const todolist = await TodoList.updateOne(
+            { _id: id },
+            { $push: { listItems: { title, done } } }
+        );
+        res.statusCode = 200;
+        res.send(todolist);
+    } catch (err) {
+        res.statusCode = 422;
+        res.send({ success: false });
+    }
+});
+
+// get all todoLists
+todolistRoute.get('/', async (req, res) => {
+    try {
+        const todolist = await TodoList.find({ userId: req.signedData.id, _id: id }).exec();
+        res.statusCode = 200;
+        res.send(todolist);
+
+    } catch (error) {
+        res.statusCode = 422;
+        res.send({ message: 'not found' });
+
+    }
+});
+
+// get all a todolist
+todolistRoute.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const todolists = await TodoList.find({ userId: req.signedData.id }).exec();
+        res.statusCode = 200;
+        res.send(todolists);
+
+    } catch (error) {
+        res.statusCode = 422;
+        res.send({ message: 'not found' });
+
+    }
+});
+
+//delete a todolist
+todolistRoute.delete('/:id', async(req, res) =>{
+    try {
+      const userId = req.signedData.id;
+      const id = req.params.id;
+      await TodoList.deleteOne({userId:userId, _id:id});
       res.statusCode = 200;
-      res.send(todolist);
+      res.send({message : 'todolist deleted'});
     } catch (err) {
       res.statusCode = 422;
-      res.send({ success: false });
+      res.send({message: 'deletion falied'});
     }
   });
 
-  // get all todoLists
-  todolistRoute.get('/', async (req, res) => {
+  //delete a todolist's item
+  todolistRoute.delete('/item/:listid/:itemid', async (req, res) => {
+    const listid = req.params.listid;
+    const itemid = req.params.itemid;
     try {
-      const todolists = await TodoList.find({ userId : req.signedData.id }).exec();
-      res.statusCode = 200;
-      res.send(todolists);
-  
-    } catch (error) {
-      res.statusCode = 422;
-      res.send({ message: 'not found' });
-  
+        const todolist = await TodoList.updateOne(
+            { _id: listid },
+            { $pull:  { listItems: { _id:  itemid} }}
+        );
+        res.statusCode = 200;
+        res.send(todolist);
+    } catch (err) {
+        res.statusCode = 422;
+        res.send({ success: false });
     }
-  });
+});
