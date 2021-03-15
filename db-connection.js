4e76dcea00888186a452c90752311bf495f1f740
Rokaia Ghareeb <Rokaiaessam97@gmail.com>
2021-03-15 13:36:29 +0200

    pk

--- db-connection.js
+++ db-connection.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://rokaia-admin:${process.env.password}@cluster0.huvkw.mongodb.net/todo`, { useFindAndModify: false }, (err)=>{
+mongoose.connect(`mongodb+srv://rokaia-admin:${process.env.password}@cluster0.i8bmm.mongodb.net/todo?retryWrites=true&w=majority&authSource=rokaia-admin`, { useFindAndModify: false }, (err)=>{
     if(err){
       console.error(err);
       process.exit(1);
